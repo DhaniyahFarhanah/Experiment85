605f193d4d1f2d46b4b91dd87c7ae53eb4167296
DhaniyahFarhanah <73376139+DhaniyahFarhanah@users.noreply.github.com>
2023-06-26 19:06:35 +0800

    having fun with transitions :D
    It's fun now but it'll be hell in the later parts

---
+++ Assets/Scenes/SceneLoader.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//===============SCENE LOADER===============
+// This script allows the scene to move onto the next scene in the build setting with a transition. Now allows immediate transitions or without the transition.
+public class SceneLoader : MonoBehaviour
+{
+    public Animator sceneTranAnimator; //animator with the transition
+    public float tranWaitTime = 1f; //time for animator to play the animation
+    
+    public bool withTran; //choice to have transition or instant change.
+    public int sceneToTransferIndex; //makes it so they can transfer to whatever scene needed.
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (withTran) //disables the transition animator if transition isn't wanted
+        {
+            sceneTranAnimator.enabled = true;
+        }
+        else
+        {
+            sceneTranAnimator.enabled = false;
+        }
+    }
+    public void LoadChosenLevel() //loads the chosen level if needed
+    {
+        if (withTran) //immediate change//with transition
+        {
+            StartCoroutine(LoadLevel(sceneToTransferIndex)); 
+        }
+
+        else //immediate change
+        {
+            SceneManager.LoadScene(sceneToTransferIndex);
+        }
+    }
+    public void LoadNextLevel() //loads next level in build settings
+    {
+        if(withTran) //with transition
+        {
+            StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1)); //retrieves the index of the current scene and adds 1, moving onto the next scene in build settings.
+        }
+
+        else //immediate change
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+        }  
+    }
+
+    //COROUTINES LETS GOOOO
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //play animation
+        sceneTranAnimator.SetTrigger("Start"); //triggers the transition animation.
+
+        yield return new WaitForSeconds(tranWaitTime);
+
+        //LoadScene
+        SceneManager.LoadScene(levelIndex); //goes to next level
+    }
+}
