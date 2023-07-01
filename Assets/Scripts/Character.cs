32b6daedbaf0b3defda657535d6c88dc6f01f61f
m4tte <47273211+m4tte@users.noreply.github.com>
2023-07-01 11:43:37 +0800

    Followed Wk 8 Demo 1 and beginning of Demo 2
    Able to obtain info from json

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+    public int charId { get; }
+    public string charName { get; }
+    public int baseStatHealth { get; }
+    public int baseStatDmg { get; }
+    public int baseStatSpeed { get; }
+    public int baseStatShotSpeed { get; }
+    public int baseStatRange { get; }
+    public int baseStateSlimeRate { get; }
+
+    public Character(int charId, string charName, int baseStatHealth, int baseStatDmg, int baseStatSpeed, int baseStatShotSpeed, int baseStatRange, int baseStateSlimeRate)
+    {
+        this.charId = charId;
+        this.charName = charName;
+        this.baseStatHealth = baseStatHealth;
+        this.baseStatDmg = baseStatDmg;
+        this.baseStatSpeed = baseStatSpeed;
+        this.baseStatShotSpeed = baseStatShotSpeed;
+        this.baseStatRange = baseStatRange;
+        this.baseStateSlimeRate = baseStateSlimeRate;
+    }
+}
